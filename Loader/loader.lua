6317470b47091d14b8a348d64bc0df24d696dd55
Vi0z <ashtoncrusher7@gmail.com>
2023-01-16 22:26:17 -0500

    loader start

--- Loader/loader.lua
+++ Loader/loader.lua
@@ -1,11 +1,10 @@
 local games = {
-    [3214114884] = "https://raw.githubusercontent.com/bppwd2024-garcia/Solar/main/Games/FlagWars.lua";
+    [32143114884] = "https://raw.githubusercontent.com/bppwd2024-garcia/Solar/main/Games/FlagWars.lua";
 }
 local universal = ""
 
 if games[game.PlaceId] ~= nil then
     loadstring(game:HttpGet(games[game.PlaceId]))()
-end
 else
-    loadstring(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/bppwd2024-garcia/Solar/main/Universal/universal.lua"))()
 end
